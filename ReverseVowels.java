4d842e7603c81be1711b008feaf265034eb1f8c9
Заитов Дмитрий <45773616+DimaZet@users.noreply.github.com>
2019-03-02 01:19:05 +0300

    Add files via upload

--- ReverseVowels.java
+++ ReverseVowels.java
@@ -4,27 +4,31 @@ public class ReverseVowels {
         String test1 = "abcdefghi";
         String test2 = "aaaiuiooo";
         String test3 = "bvcnmm";
+        String test4 = "eyuio";
         test1 = reverse(test1);
         System.out.println(test1);
         System.out.println(reverse(test2));
         System.out.println(reverse(test3));
+        System.out.println(reverse(test4));
     }
 
     public static String reverse(String word) {
         char[] _word = word.toCharArray();
         String dictionary = "eyuioa";
         String vowels = "";
-
-        for(int i=0;i<word.length();++i)
+        int end = word.length()-1;
+        for(int i=0;i<=end;++i)
             if( dictionary.indexOf( _word[i] ) != -1 ) {
-                vowels += _word[i];
-                _word[i] = '_';
+                for(int j=end;j>i;--j) {
+                    if( dictionary.indexOf( _word[j] ) != -1  && i != j) {
+                        char temp = _word[i];
+                        _word[i] = _word[j];
+                        _word[j] = temp;
+                        end = --j;
+                        break;
+                    }
+                }
             }
-
-        int count = vowels.length()-1;
-        for(int i=0;i<word.length();++i)
-            if( _word[i] == '_')
-                _word[i] = vowels.charAt(count--);
 
         return new String(_word);
     }
