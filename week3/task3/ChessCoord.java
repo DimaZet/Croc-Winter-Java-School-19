49495de001abe7c73a6eee27d220a129a0c8f0a6
Заитов Дмитрий <45773616+DimaZet@users.noreply.github.com>
2019-03-14 00:02:18 +0300

    Add files via upload

---
+++ week3/task3/ChessCoord.java
@@ -0,0 +1,43 @@
+package croc.week3.task3;
+
+public class ChessCoord {
+    private int x;  //1..8
+    private int y;  //1..8
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        if (x < 1 || x > 8)
+            throw new IllegalArgumentException("set illegal x");
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        if (y < 1 || y > 8)
+            throw new IllegalArgumentException("set illegal y");
+        this.y = y;
+    }
+
+    public ChessCoord(int x, int y) {
+        setX(x);
+        setY(y);
+    }
+
+    public ChessCoord(String parse) {
+        if (parse == null || parse.length() != 2 || parse.charAt(0) < 'a' || parse.charAt(1) > 'h')
+            throw new IllegalArgumentException("illegal parse argument");
+        x = parse.charAt(0)-'a'+1;
+        y = parse.charAt(1)-'0';
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf((char)('a'-1+x)) + y;
+    }
+}
---
+++ week3/task3/Main.java
@@ -0,0 +1,9 @@
+package croc.week3.task3;
+
+public class Main {
+    public static void main(String[] args) {
+        ChessCoord a = new ChessCoord(3,5);
+        ChessCoord b = new ChessCoord("g3");
+        System.out.println(a + " " + b);
+    }
+}
