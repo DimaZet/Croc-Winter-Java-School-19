238993e029b941f404b3541ff065a8891279711c
Заитов Дмитрий <45773616+DimaZet@users.noreply.github.com>
2019-04-08 00:14:11 +0300

    Add files via upload

--- week6/LongestCallsInLog/Main.java
+++ week6/LongestCallsInLog/Main.java
@@ -6,12 +6,12 @@ import java.util.Collections;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        String filepath = args[0];
+        final String FILE_PATH = args[0];
+        final int CALL_COUNT = 3;
 
-        ArrayList<Call> log = new ArrayList<>();
-        log = LogParser.parse(filepath);
+        ArrayList<Call> log = LogParser.parse(FILE_PATH);
         Collections.sort(log, Collections.reverseOrder());
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < CALL_COUNT; i++) {
             System.out.println(log.get(i));
         }
     }
